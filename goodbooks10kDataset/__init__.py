2778a419a17203b612cdd8b5e9c4bc2cc266cde5
Ufkun-Bayram Menderes <56983638+ufkunmndrs@users.noreply.github.com>
2021-04-11 21:54:02 +0200

    Add files via upload

---
+++ goodbooks10kDataset/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 11 19:53:33 2020
+
+@author: Ufkun-Bayram Menderes
+"""
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
